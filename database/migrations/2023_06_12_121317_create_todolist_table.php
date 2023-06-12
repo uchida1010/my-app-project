2f4a35ef316a23388c53cab8a47a9c366b164517
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-06-12 22:06:50 +0900

    migration create

---
+++ database/migrations/2023_06_12_121317_create_todolist_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolist', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->string('rank',100);
+            $table->date('deadline');
+            $table->date('schedule');
+            $table->integer('progress');
+            $table->string('others',1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolist');
+    }
+};
