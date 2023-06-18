56b2e1b466358b62e1c2c5f9df2ee179b1d79dad
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-06-18 17:04:48 +0900

    migration table_rename err

--- database/migrations/2023_06_12_121317_create_todos_table.php
+++ database/migrations/2023_06_12_121317_create_todos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('todolist', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name',100);
             $table->string('rank',100);
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('todolist');
+        Schema::dropIfExists('todos');
     }
 };
