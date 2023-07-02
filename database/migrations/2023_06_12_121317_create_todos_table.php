ddd3c56ef81ce4442a848731f3500d136ec35ec2
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-07-02 22:12:27 +0900

    todos err

--- database/migrations/2023_06_12_121317_create_todos_table.php
+++ database/migrations/2023_06_12_121317_create_todos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('todos', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->foreignId('user_id');
             $table->string('name',100);
             $table->string('rank',100);
