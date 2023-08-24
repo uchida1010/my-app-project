b0e8e258492b29aa99243ec970dc6f9a7c25061a
uchida1010 <tomoki.ksu.0826@gmail.com>
2023-08-24 21:36:05 +0900

    update Breeze ログイン画面生成

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoListController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,8 +9,8 @@ use App\Http\Controllers\TodoListController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -18,8 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-    Route::prefix('todolist')->group(function () {
-        Route::get('/', [TodoListController::class, 'index']);
-        Route::get('create',[TodoListController::class, 'showCreateTodo']);
-        Route::post('create',[TodoListController::class, 'executeCreateTodo']);
-    });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
